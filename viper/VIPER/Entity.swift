e3efd075d6d23254d6ccca1baa2cf37b1e249974
RinStar2003 <mituha133@gmail.com>
2022-12-02 18:28:48 +0200

    Initial Commit

---
+++ viper/VIPER/Entity.swift
@@ -0,0 +1,14 @@
+//
+//  Entity.swift
+//  viper
+//
+//  Created by мас on 02.12.2022.
+//
+
+import Foundation
+
+// Cool name for the model, as I understand
+
+struct User: Codable {
+    var name: String
+}
---
+++ viper/VIPER/View.swift
@@ -0,0 +1,87 @@
+//
+//  View.swift
+//  viper
+//
+//  Created by мас on 02.12.2022.
+//
+
+import Foundation
+import UIKit
+
+protocol AnyView {
+    var presenter: AnyPresenter? { get set }
+    
+    func update(with users: [User])
+    func update(with error: String)
+}
+
+class UserViewController: UIViewController, AnyView, UITableViewDataSource, UITableViewDelegate {
+
+    var presenter: AnyPresenter?
+    
+    private let tableView: UITableView = {
+       
+        var table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.isHidden = true
+        return table
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.isHidden = true
+        return label
+    }()
+    
+    var users: [User] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(label)
+        view.addSubview(tableView)
+        view.backgroundColor = .systemBlue
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+        label.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        label.center = view.center
+    }
+    
+    func update(with users: [User]) {
+        
+        DispatchQueue.main.async {
+            print("sucsess")
+            self.users = users
+            self.tableView.reloadData()
+            self.tableView.isHidden = false
+        }
+        
+    }
+    
+    func update(with error: String) {
+        
+        DispatchQueue.main.async {
+            print(error)
+            self.users = []
+            self.tableView.isHidden = true
+            self.label.isHidden = false
+        }
+    }
+    
+    // MARK: TABLEVIEW
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].name
+        return cell
+    }
+}
